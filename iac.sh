06f06504d8848acfeac0bc272686a6822e39e179
bianca-nf <100863677+bianca-nf@users.noreply.github.com>
2022-08-12 20:09:13 -0300

    Add files via upload

---
+++ iac.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+echo "Criando diretórios no sistema..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven/
+mkdir /sec
+
+echo "Criando grupos de usuários no sistema..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários no sistema..."
+
+useradd carlos -c "Carlos" -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_ADM
+useradd maria -c "Maria" -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_ADM
+useradd joao -c "João" -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_ADM
+
+useradd debora -c "Débora" -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_VEN
+useradd sebastiana -c "Sebastiana" -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_VEN
+useradd roberto -c "Roberto" -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_VEN
+
+useradd josefina -c "Josefina" -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_SEC
+useradd amanda -c "Amanda" -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_SEC
+useradd rogerio -c "Rogério" -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_SEC
+
+echo "Parametrizando política de permissões dos diretórios..."
+
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 777 /publico
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec 
+
+echo "Usuários , diretórios e permissões criadas..."
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+D
